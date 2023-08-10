ac63812c651781d5edd068e7fabf79d3e974e44a
rajkmajkdev <rajkmajkdev@gmail.com>
2023-08-10 13:51:09 -0400

    test

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# gh-automatic
+# fake-it-til-you-make-it
+
+Have you come across to someone that thinks you don't deserve
+a job because you don't have GitHub contributions?
+
+Run this program.
+
+## Usage
+
+
+For example, commit 10 commits max every day for two years,
+and avoid committing on Saturday and Sunday:
+
+```
+go run main.go -max=10 -skip-weekend=true
+```
+## Why?
+
+I obviously built this tool as a joke and don't encourage anyone
+to use it. It's a demonstration that GitHub contributions can be faked,
+and they are not a measure.
+Please leave developers alone and let them focus on important problems
+instead of wasting time gaming the system.
---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"math/rand"
+	"os/exec"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+var (
+	max         int
+	skipWeekend bool
+	years       int
+)
+
+func main() {
+	flag.IntVar(&max, "max", 5, "Max number of commits per day")
+	flag.BoolVar(&skipWeekend, "skip-weekend", true, "Set false if you don't want to skip weekend")
+	flag.IntVar(&years, "years", 2, "Number of years to commit")
+	flag.Parse()
+
+	end := time.Now()
+	start := end.AddDate(-1*years, 0, 0)
+
+	for d := start; d.Before(end); d = d.AddDate(0, 0, 1) {
+		if skipWeekend {
+			if d.Weekday() == time.Saturday || d.Weekday() == time.Sunday {
+				continue
+			}
+		}
+
+		commits := rand.Intn(max)
+		log.Printf("Committing %d times on %s", commits, d)
+
+		for i := 0; i < commits; i++ {
+			cmd := exec.Command(
+				"git",
+				"commit",
+				"--allow-empty",
+				"--date", d.Format("Mon Jan 02 15:04:05 -0700 2006"),
+				"-m", fmt.Sprintf("Commit from %s", d.Format("2006-01-02")))
+			if err := cmd.Run(); err != nil {
+				log.Printf("Error when committing: %v", err)
+			}
+		}
+	}
+}
